8a0f3618039e1868ef6ca77d8ee54430d6b81108
Marcelino Eilert <marcelinoeilert@outlook.com>
2020-06-03 02:05:48 -0400

    feature/relacionamento users-appointments

--- src/database/migrations/1590633172675-CreateAppointments.ts
+++ src/database/migrations/1590633172675-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1590633172675
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
