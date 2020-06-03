eb8abe9244bf5cccd80327000eb87350539a2e54
Marcelino Eilert <marcelinoeilert@outlook.com>
2020-06-03 01:11:42 -0400

    feature/criando repositorio appointments

--- src/database/migrations/1590633172675-CreateAppointments.ts
+++ src/database/migrations/1590633172675-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1590633172675
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
